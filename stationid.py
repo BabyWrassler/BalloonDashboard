2985e9581ba41113d3e56453552b80b616773b09
Jeff Highsmith <junk@jeffhighsmith.com>
2013-05-21 11:53:19 -0400

    fixed widths

--- stationid.py
+++ stationid.py
@@ -11,7 +11,7 @@ import cgi
 
 def openDB():
   global con
-  DB_FILENAME = "/home/pi/10DOFii/10DOF_data.db"
+  DB_FILENAME = "../data/10DOF_data.db"
   con = db.connect(DB_FILENAME)
 
 openDB()
@@ -38,24 +38,22 @@ template += "</head><body>\n"
 template += '<div id="content">\n'
 
 # ENTER CALL SIGN HERE:
-template += '<div id="page-header"><h1>WJ3FF-3</h1></div>'
+template += '<div id="page-header"><h2>NSL-10 / AK4CH-11 / Travel Gnome in Space!</h2></div>'
 
 template += '<img id="idimage" src="latest.jpg">'
 
 template += '<div id="statbox">'
 
-template += "<h2>NSL-10</h2>"
-
 template += '<p id="odd">Time: '
-template += (rows[0][3])[11:] # Chopping off date
+template += (rows[0][3])[11:19] # Chopping off date
 template += "</p>\n"
 
 template += '<p id="even">B Alt: '
-template += str(rows[0][20])
+template += str(rows[0][20])[:10]
 template += "</p>\n"
 
 template += '<p id="odd">GPS Alt: '
-template += str(rows[0][2])
+template += str(rows[0][2])[:10]
 template += "</p>\n"
 
 template += '<p id="even">Lat: '
